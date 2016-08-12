9d74b2241009c03c3659f92f43e86d44161be647
ping <ping@DESKTOP-BNROML3>
2016-08-12 20:17:41 +0800

    11

--- src/main/java/com/webank/blockchain/main/BlockChainController.java
+++ src/main/java/com/webank/blockchain/main/BlockChainController.java
@@ -5,15 +5,16 @@ import java.net.InetAddress;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JsonConfig;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.webank.blockchain.domain.Block;
 import com.webank.blockchain.domain.BlockChain;
@@ -25,8 +26,8 @@ public class BlockChainController {
 
 	Block bc = new Block(0,0);
 	Block lastBc = bc;
-
 	BlockChain bco = new BlockChain(bc);
+    static ConcurrentHashMap<String,List> ip_records = new ConcurrentHashMap<>();
 	@CrossOrigin
 	@RequestMapping(value = "/queryBlock", method = RequestMethod.GET)
 	public String bc() {
@@ -43,6 +44,7 @@ public class BlockChainController {
 		//Record l = new Record(1,100,new Timestamp(System.currentTimeMillis()),str,"10.106.11.22");
 		
 		JSONObject jsonobj = JSONObject.fromObject(str,new JsonConfig());
+        String ip = (String)jsonobj.get("ip");
 		Record r = new Record();
 //			DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 //			String generateTime = sdf.format(time);
@@ -58,7 +60,14 @@ public class BlockChainController {
 		bc = bco.getBlock();
 		//String res = JsonTools.createJsonString("lastBlock", bc);
 		//return bc.toJson();
-		return "success";
+        //按ip将Record分类，一个特定的ip对应在该ip下请求的Records
+        List list = ip_records.get(ip);
+        if(list==null){
+            list = new ArrayList<Record>();
+            ip_records.put(ip,list);
+        }
+        list.add(r);
+        return "success";
 	}
 
 //	@CrossOrigin
@@ -93,7 +102,7 @@ public class BlockChainController {
 	@RequestMapping(value = "/addBlock", method = RequestMethod.POST)
 	public String add(@RequestBody String requestJson) throws UnsupportedEncodingException {
 		String str = java.net.URLDecoder.decode(requestJson,"utf-8");
-		JSONObject outer_jsonobj = JSONObject.fromObject(str,new JsonConfig());;
+        JSONObject outer_jsonobj = JSONObject.fromObject(str,new JsonConfig());;
 		JSONObject jsonobj = JSONObject.fromObject(outer_jsonobj.get("msgBody"));
 		String ip = "";
 		String result = "";
@@ -110,17 +119,31 @@ public class BlockChainController {
 			r.setTime(time);
 			r.setRemark((String) jsonobj.get("remark"));
 			r.setIp(ip);
-			result += Client.sendPost("http://119.29.98.174:8080/pushAddRequest", r.toString());
-			result += Client.sendPost("http://119.29.98.174:8081/pushAddRequest", r.toString());
-			result += Client.sendPost("http://119.29.98.174:8082/pushAddRequest", r.toString());
-			
-//		Client.sendPost("http://localhost:8081/pushAddRequest", jsonobj);
-//		Client.sendPost("http://localhost:8082/pushAddRequest", jsonobj);
+			result += Client.sendPost("http://127.0.0.1:8080/pushAddRequest", r.toString());
+//			result += Client.sendPost("http://119.29.98.174:8081/pushAddRequest", r.toString());
+//			result += Client.sendPost("http://119.29.98.174:8082/pushAddRequest", r.toString());
 		}
 		catch (Exception e){
 			System.out.println(e);
 		}
 		return result;
 	}
+
+
+    @CrossOrigin
+    @RequestMapping(value = "/queryRecordByIP", method = RequestMethod.POST)
+    public String getRecordList(@RequestParam("ip") String ip) throws UnsupportedEncodingException {
+        List list = ip_records.get(ip);
+        Iterator it = list.iterator();
+        String res = "[";
+        while(it.hasNext()) {
+            res = res +  ((Record)it.next()).toString()+",";
+        }
+        if(list.size()>0) {
+            res = res.substring(0,res.length()-1);
+        }
+        res += "]";
+        return res;
+    }
 	
 }
